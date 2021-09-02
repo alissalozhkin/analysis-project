d149bd945528f8c179d302c36b7fa9fec11f824f
alissalozhkin <65978304+alissalozhkin@users.noreply.github.com>
2021-09-02 11:31:59 -0400

    store title, description, and rating

---
+++ github/accesso_app/mall_info.js
@@ -0,0 +1,6 @@
+const mallData = [
+    {'title': 'Bayview Village', 'description': 'Bayview Village is a mall located in North York with over 110 stores. Â\xa0There are several stores that are quite accessible.'},
+    {'title': 'CF Shops at Don Mills', 'description': 'This outdoor mall is located in North York and has many retail stores and many restaurants. A large portion of stores do not have buttons, so we suggest shopping here with a friend.'},
+    {'title': 'East York Town Centre', 'description': 'East York Town Centre is a single floored spacious mall with many stores accessible from inside and outside the mall. However, it is difficult to access some of the stores from the outside. Unfortunately, we were not given permission to collect data from a number of stores in this mall.'},
+    
+]
